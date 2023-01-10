8345f2a4e1eb3f8bb72e511a587cf79de87a910e
leelingzhen <lee.lingzhenn+github@gmail.com>
2023-01-10 15:30:23 +0800

    removed wandb logger hook, no support for hpc

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -5,11 +5,11 @@ log_config = dict(
         dict(type='TextLoggerHook', by_epoch=False),
         # dict(type='TensorboardLoggerHook')
         # dict(type='PaviLoggerHook') # for internal services
-        dict(type='MMSegWandbHook', by_epoch=False, # The Wandb logger is also supported, It requires `wandb` to be installed.
-             init_kwargs={'entity': "leelingzhen", # The entity used to log on Wandb
-                          'project': "mmsegmentation", # Project name in WandB
-                          }), # Check https://docs.wandb.ai/ref/python/init for more init arguments.
-        # MMSegWandbHook is mmseg implementation of WandbLoggerHook. ClearMLLoggerHook, DvcliveLoggerHook, MlflowLoggerHook, NeptuneLoggerHook, PaviLoggerHook, SegmindLoggerHook are also supported based on MMCV implementation.
+        # dict(type='MMSegWandbHook', by_epoch=False, # The Wandb logger is also supported, It requires `wandb` to be installed.
+        #      init_kwargs={'entity': "leelingzhen", # The entity used to log on Wandb
+        #                   'project': "mmsegmentation", # Project name in WandB
+        #                   }), # Check https://docs.wandb.ai/ref/python/init for more init arguments.
+        # # MMSegWandbHook is mmseg implementation of WandbLoggerHook. ClearMLLoggerHook, DvcliveLoggerHook, MlflowLoggerHook, NeptuneLoggerHook, PaviLoggerHook, SegmindLoggerHook are also supported based on MMCV implementation.
     ])
 # yapf:enable
 dist_params = dict(backend='nccl')
